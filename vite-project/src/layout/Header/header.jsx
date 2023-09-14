84ff676fa533749ba2f6414cc6b0343e7a0b1959
Pierre Pillot <pierrepillot0110@gmail.com>
2023-09-14 18:40:37 +0200

    mise en place de la nav

--- vite-project/src/layout/Header/header.jsx
+++ vite-project/src/layout/Header/header.jsx
@@ -4,7 +4,16 @@ import mer from "../../assets/mer.jpg"
 function Header() {
     return (
         <header>
-            <img src={mer} alt="" />
+            <img className="sea" src={mer} alt="plage au bord de mer" />
+            <nav>
+                <h1 className="logo">Hello</h1>
+                <ul className="nav">
+                    <li className="nav--container">À propos</li>
+                    <li className="nav--container">Mes projets</li>
+                    <li className="nav--container">Mes services</li>
+                    <li className="nav--container">Contact</li>
+                </ul>
+            </nav>
         </header>
     )
 }
