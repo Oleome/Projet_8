34729bab0879f8340545ad1491b9c6f919decf16
Bilou <pierrepillot0110@gmail.com>
2023-09-20 14:58:43 +0200

    work on cards

--- vite-project/src/pages/projets.jsx
+++ vite-project/src/pages/projets.jsx
@@ -1,6 +1,17 @@
+import kasa from '../assets/Kasa.png';
+
 function Projets () {
     return (
-        <h2>Coucou</h2>
+        <main>
+            <div className="card"><img src={kasa} alt="" /></div>
+            <div className="card"><img src="" alt="" /></div>
+            <div className="card"><img src="" alt="" /></div>
+            <div className="card"><img src="" alt="" /></div>
+            <div className="card"><img src="" alt="" /></div>
+            <div className="card"><img src="" alt="" /></div>
+            <div className="card"><img src="" alt="" /></div>
+            <div className="card"><img src="" alt="" /></div>
+        </main>
     )
 }
 
