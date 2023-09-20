91c7a728c386ea4ff5f009b3a97bed27de917e82
Pierre Pillot <pierrepillot0110@gmail.com>
2023-09-20 19:42:12 +0200

    travail sur les pastilles

--- vite-project/src/pages/projets.jsx
+++ vite-project/src/pages/projets.jsx
@@ -1,18 +1,47 @@
 import kasa from '../assets/Kasa.png';
-import grimoire from '../assets/monvieuxgrimoire.png'
+import grimoire from '../assets/monvieuxgrimoire.png';
 import '../styles/projet.scss';
+import react from '../assets/react.png';
 
 function Projets () {
     return (
         <main className='main--projet'>
-           <div className="card"><img src={kasa} alt="" /></div>
-            <div className="card"><img src={grimoire} alt="" /></div>
-            <div className="card"><img src="" alt="" /></div>
-            <div className="card"><img src="" alt="" /></div>
-            <div className="card"><img src="" alt="" /></div>
-            <div className="card"><img src="" alt="" /></div>
-            <div className="card"><img src="" alt="" /></div>
-            <div className="card"><img src="" alt="" /></div>   
+            <div className="card">
+                <img className='card--img' src={kasa} alt="" />
+                <div className='pastille'>
+                    <img className='pastille--tech' src={react} alt="" />
+                </div>
+            </div>
+            <div className="card">
+                <img className='card--img' src={grimoire} alt="" />
+                <div className='pastille'>
+                    <img className='pastille--tech' src={react} alt="" />
+                </div>
+            </div>
+            <div className="card">
+                <img className='card--img' src={grimoire} alt="" />
+                <div className='pastille'>
+                    <img className='pastille--tech' src={react} alt="" />
+                </div>
+            </div>
+            <div className="card">
+                <img className='card--img' src={grimoire} alt="" />
+                <div className='pastille'>
+                    <img className='pastille--tech' src={react} alt="" />
+                </div>
+            </div>  
+            <div className="card">
+                <img className='card--img' src={grimoire} alt="" />
+                <div className='pastille'>
+                    <img className='pastille--tech' src={react} alt="" />
+                </div>
+            </div>  
+            <div className="card">
+                <img className='card--img' src={grimoire} alt="" />
+                <div className='pastille'>
+                    <img className='pastille--tech' src={react} alt="" />
+                </div>
+            </div>
         </main>
     )
 }
