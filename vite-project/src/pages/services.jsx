21a00d0a3088a1f6a4c184704a28a6b773c835f1
Pierre Pillot <pierrepillot0110@gmail.com>
2023-09-21 14:05:55 +0200

    card services

--- vite-project/src/pages/services.jsx
+++ vite-project/src/pages/services.jsx
@@ -1,6 +1,23 @@
+import icone from '../assets/icone.png';
+import '../styles/services.scss';
+
 function Services () {
     return (
-        <h2>Coucou</h2>
+        <main className='services--container'>
+            <div className="card">
+                <div className='icone'>
+                    <img src={icone} alt="icone d'ordinateur" />
+                </div>
+                <div className='skills'>
+                    <h2>Développement web</h2>
+                    <p></p>
+                </div>
+                <div className='languages'>
+                    <h2>Languages utilisés</h2>
+                    <p></p>
+                </div>
+            </div>
+        </main>
     )
 }
 
