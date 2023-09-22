341e4bb1f7276efaade16254f60723db6a2cb64d
Pierre Pillot <pierrepillot0110@gmail.com>
2023-09-22 14:10:31 +0200

    apropos

--- vite-project/src/pages/a_propos.jsx
+++ vite-project/src/pages/a_propos.jsx
@@ -1,6 +1,13 @@
+import '../styles/apropos.scss';
+
 function APropos () {
     return (
-        <h2>Coucou</h2>
+        <p className="container">
+            <span>Passionné de développement web, je suis actuellement à la recherche d'une première mission après ma formation avec Openclassrooms.</span>
+            <span>J'aime particulièrement le développement web car c'est un milieu en perpétuel évolution qui permet un cadre de travail stimulant et un apprentissage continu.</span>
+            <span>Je suis également passionné de kayak et de pêche en mer, activités qui me permettent de m'épanouir et d'être créatif dans mon métier.</span>
+            <span>J'ai pour but de devenir freelance et d'être un soutient pour les entreprises et les professionnels avec lesquels je travaillerais.</span>
+        </p>      
     )
 }
 
